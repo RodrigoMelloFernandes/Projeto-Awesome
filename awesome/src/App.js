4f6e868358176dea2bfad1d9730e9a1ba8942cf4
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-07-25 17:14:09 -0300

    add scss in componentes, and reset css

--- awesome/src/App.js
+++ awesome/src/App.js
@@ -1,10 +1,22 @@
 import Header from "./Components/_1_Header";
+import Main from './Components/_2_Main';
+import WeAreAwesome from './Components/_3_Section_WeAreAwesome';
+import AwesomeTeam from './Components/_4_Section_AwesomeTeam';
+import OurAwesomeThings from './Components/_5_Section_OurAwesomeThings'
+import AwesomePortfolio from './Components/_6_Section_AwesomePortfolio'
+import ContactAwesome from './Components/_7_Footer_ContactAwesome'
 
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Main></Main>
+      <WeAreAwesome></WeAreAwesome>
+      <AwesomeTeam></AwesomeTeam>
+      <OurAwesomeThings></OurAwesomeThings>
+      <AwesomePortfolio></AwesomePortfolio>
+      <ContactAwesome></ContactAwesome>
     </>
   );
 }
--- awesome/src/Components/_2_Main/index.jsx
+++ awesome/src/Components/_2_Main/index.jsx
@@ -1,8 +1,8 @@
-
+import styles from './Main.module.scss'
 
 function Main () {
     return (
-        <main>
+        <main className={styles.main}>
             <h2 >We Make templates that aew <span>awesome</span></h2>
             <p>Download, edit and aplly this awesome for yout website</p>
 
