51761c5c304831d25358d43263c7d4f1ad309b90
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-07-25 17:04:10 -0300

    Section OurAwesomeThings html ok

---
+++ awesome/src/Components/_5_Section_OurAwesomeThings/index.jsx
@@ -0,0 +1,36 @@
+import { FaLaptop, FaCloud, FaCog } from 'react-icons/fa'
+
+function OurAwesomeThings () {
+    return (
+        <section>
+            <h2>our <span>awesome</span> things</h2>
+
+            <div>
+                <div>
+                    <FaLaptop/>
+                </div>
+                <h2>Web Design</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie. Adipiscing vitae vel quam proin eget mauris eget. Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie.</p>
+            </div>
+
+            <div>
+                <div>
+                    <FaCloud/>
+                </div>
+                <h2>Cloud Computing</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie. Adipiscing vitae vel quam proin eget mauris eget. Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie.</p>
+            </div>
+
+            <div>
+                <div>
+                    <FaCog/>
+                </div>
+                <h2>Ux Design</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie. Adipiscing vitae vel quam proin eget mauris eget. Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie.</p>
+            </div>
+
+        </section>
+    )
+}
+
+export default OurAwesomeThings
