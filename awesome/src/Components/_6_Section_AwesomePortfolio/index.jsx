eedd7734ebbd0c39aac5d47ef4d7de11b63b47b1
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-07-25 17:04:54 -0300

    Section Awesome Portofolio ok

---
+++ awesome/src/Components/_6_Section_AwesomePortfolio/index.jsx
@@ -0,0 +1,20 @@
+import styles from './AwesomePortfolio.module.scss'
+
+function AwesomePortfolio () {
+    return (
+        <section>
+            
+            <div className={styles.img1}></div>
+            <div className={styles.img2}></div>
+            <div className={styles.img3}></div>
+            <div className={styles.img4}></div>
+
+            <div className={styles.img3}></div>
+            <div className={styles.img4}></div>
+            <div className={styles.img2}></div>
+            <div className={styles.img1}></div>
+        </section>
+    )
+}
+
+export default AwesomePortfolio
