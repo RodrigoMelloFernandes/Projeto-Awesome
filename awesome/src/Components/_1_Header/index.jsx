dae7c8b4bf6138138cadf89784af369b5f91b3f0
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-07-26 10:32:37 -0300

    Header Scss almoust done

--- awesome/src/Components/_1_Header/index.jsx
+++ awesome/src/Components/_1_Header/index.jsx
@@ -5,51 +5,52 @@ import styles from './Header.module.scss';
 function Header () {
     return (
         <header className={styles.header}>
-            <nav>
-                <h2 className={styles.header__title}>Awesome</h2>
-                <div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
+            <nav className={styles.header__menu}>
+                <h2 className={styles.header__menu___title}>Awesome</h2>
 
-                <div>
-                    <li>
-                        <ul>
-                            <a href="">Home</a>
-                        </ul>
-                        <ul>
-                            <a href="">About</a>
-                        </ul>
-                        <ul>
-                            <a href="">Team</a>
-                        </ul>
-                        <ul>
-                            <a href="">Service</a>
-                        </ul>
-                        <ul>
-                            <a href="">Portfolio</a>
-                        </ul>
-                        <ul>
-                            <a href="">Contact</a>
-                        </ul>
-                    </li>
-                </div>
+
+                {/* <div>
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">About</a>
+                        </li>
+                        <li>
+                            <a href="#">Team</a>
+                        </li>
+                        <li>
+                            <a href="#">Service</a>
+                        </li>
+                        <li>
+                            <a href="#">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div> */}
             </nav>
-
-            
-            <div> 
-                <FaPhoneAlt/>
-                <h3>Phone</h3>
-                <p>010-020-0340</p>
-                <FaEnvelope/>
-                <h3>Email</h3>
-                <p>awesome@company.com</p>
-                <h3>Meet us on</h3>
-                <FaFacebookF/>
-                <FaTwitter/>
-                <FaInstagram/>
-                <FaApple/>
+      
+            <div className={styles.header__contacts}> 
+                <div className={styles.header__contacts___containerPhone}>
+                    <FaPhoneAlt className={styles.header__contacts___containerPhone____phoneIcon}/>
+                    <h3 className={styles.header__contacts___containerPhone____title}>Phone</h3>
+                    <p className={styles.header__contacts___containerPhone____subtitle}>010-020-0340</p>
+                </div>
+                <div className={styles.header__contacts___containerEmail}>
+                    <FaEnvelope className={styles.header__contacts___containerEmail____emailIcon}/>
+                    <h3 className={styles.header__contacts___containerEmail____title}>Email</h3>
+                    <p className={styles.header__contacts___containerEmail____subtitle}>awesome@company.com</p>
+                </div>
+                <div className={styles.header__contacts___containerSocialMedia}>
+                    <h3 className={styles.header__contacts___containerSocialMedia____title}>Meet us on</h3>
+                    <FaFacebookF className={styles.header__contacts___containerSocialMedia____icon}/>
+                    <FaTwitter className={styles.header__contacts___containerSocialMedia____icon}/>
+                    <FaInstagram className={styles.header__contacts___containerSocialMedia____icon}/>
+                    <FaApple className={styles.header__contacts___containerSocialMedia____icon}/>
+                </div>
             </div>
         </header>
     )
