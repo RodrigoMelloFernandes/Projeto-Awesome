38c56842dbf3c7a5d812f8cbd35b5ddf327cb2a2
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-07-26 11:25:16 -0300

    Main Scss almoust done

--- awesome/src/Components/_2_Main/index.jsx
+++ awesome/src/Components/_2_Main/index.jsx
@@ -3,10 +3,10 @@ import styles from './Main.module.scss'
 function Main () {
     return (
         <main className={styles.main}>
-            <h2 >We Make templates that aew <span>awesome</span></h2>
-            <p>Download, edit and aplly this awesome for yout website</p>
+            <h2 className={styles.main__title}>We Make templates that are <span className={styles.main__title___span}>awesome</span></h2>
+            <p className={styles.main__subtitle}>Download, edit and aplly this awesome for yout website</p>
 
-            <button>Get Started</button>
+            <button className={styles.main__button}>Get Started</button>
         </main>
     )
 }
