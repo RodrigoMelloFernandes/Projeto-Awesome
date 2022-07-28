3770ebc51c7fb02798780cdee7dec83c04ef1f4b
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-07-28 16:56:01 -0300

    AwesomeTeam Section Desktop Ok

--- awesome/src/Components/_4_Section_AwesomeTeam/index.jsx
+++ awesome/src/Components/_4_Section_AwesomeTeam/index.jsx
@@ -9,29 +9,31 @@ function AwesomeTeam () {
         <section className={styles._4}>
             <h2 className={styles._4__title}><span className={styles._4__title___span}>awesome</span> team</h2>
 
-            <div className={styles._4__container}>
-                <img className={styles._4__container___img} src={teamimg1} alt="teamimg1" />
-                <h2 className={styles._4__container___title}>tracy</h2>
-                <h3 className={styles._4__container___subtitle}>Designer</h3>
-                <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
-            </div>
-            <div className={styles._4__container}>
-                <img className={styles._4__container___img} src={teamimg2} alt="teamimg2" />
-                <h2 className={styles._4__container___title}>mary</h2>
-                <h3 className={styles._4__container___subtitle}>Developer</h3>
-                <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
-            </div>
-            <div className={styles._4__container}>
-                <img className={styles._4__container___img} src={teamimg3} alt="teamimg3" />
-                <h2 className={styles._4__container___title}>julia</h2>
-                <h3 className={styles._4__container___subtitle}>Director</h3>
-                <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
-            </div>
-            <div className={styles._4__container}>
-                <img className={styles._4__container___img} src={teamimg4} alt="teamimg4" />
-                <h2 className={styles._4__container___title}>linda</h2>
-                <h3 className={styles._4__container___subtitle}>Manager</h3>
-                <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+            <div className={styles._4__containerDesktop}>
+                <div className={styles._4__container}>
+                    <img className={styles._4__container___img} src={teamimg1} alt="teamimg1" />
+                    <h2 className={styles._4__container___title}>tracy</h2>
+                    <h3 className={styles._4__container___subtitle}>Designer</h3>
+                    <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+                </div>
+                <div className={styles._4__container}>
+                    <img className={styles._4__container___img} src={teamimg2} alt="teamimg2" />
+                    <h2 className={styles._4__container___title}>mary</h2>
+                    <h3 className={styles._4__container___subtitle}>Developer</h3>
+                    <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+                </div>
+                <div className={styles._4__container}>
+                    <img className={styles._4__container___img} src={teamimg3} alt="teamimg3" />
+                    <h2 className={styles._4__container___title}>julia</h2>
+                    <h3 className={styles._4__container___subtitle}>Director</h3>
+                    <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+                </div>
+                <div className={styles._4__container}>
+                    <img className={styles._4__container___img} src={teamimg4} alt="teamimg4" />
+                    <h2 className={styles._4__container___title}>linda</h2>
+                    <h3 className={styles._4__container___subtitle}>Manager</h3>
+                    <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
+                </div>
             </div>
         </section>
     )
