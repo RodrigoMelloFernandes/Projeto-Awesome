edb4f83afca6b49763955d39f28ed1d044d5ccbd
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-07-28 17:39:03 -0300

    ourAwesomeThings in progress

--- awesome/src/Components/_5_Section_OurAwesomeThings/index.jsx
+++ awesome/src/Components/_5_Section_OurAwesomeThings/index.jsx
@@ -6,28 +6,28 @@ function OurAwesomeThings () {
         <section className={styles._5}>
             <h2 className={styles._5__title}>our <span className={styles._5__title___span}>awesome</span> things</h2>
 
-            <div className={styles._5__container}>
-                <div className={styles._5__container___containerIcon}>
-                    <FaLaptop className={styles._5__container___containerIcon____icon}/>
+            <div className={styles._5__containerDesktop}>
+                <div className={styles._5__container}>
+                    <div className={styles._5__container___containerIcon}>
+                        <FaLaptop className={styles._5__container___containerIcon____icon}/>
+                    </div>
+                    <h2 className={styles._5__container___title}>Web Design</h2>
+                    <p className={styles._5__container___subtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie. Adipiscing vitae vel quam proin eget mauris eget. Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie.</p>
                 </div>
-                <h2 className={styles._5__container___title}>Web Design</h2>
-                <p className={styles._5__container___subtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie. Adipiscing vitae vel quam proin eget mauris eget. Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie.</p>
-            </div>
-
-            <div className={styles._5__container}>
-                <div className={styles._5__container___containerIcon}>
-                    <FaCloud className={styles._5__container___containerIcon____icon}/>
+                <div className={styles._5__container}>
+                    <div className={styles._5__container___containerIcon}>
+                        <FaCloud className={styles._5__container___containerIcon____icon}/>
+                    </div>
+                    <h2 className={styles._5__container___title}>Cloud Computing</h2>
+                    <p className={styles._5__container___subtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie. Adipiscing vitae vel quam proin eget mauris eget. Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie.</p>
                 </div>
-                <h2 className={styles._5__container___title}>Cloud Computing</h2>
-                <p className={styles._5__container___subtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie. Adipiscing vitae vel quam proin eget mauris eget. Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie.</p>
-            </div>
-
-            <div className={styles._5__container}>
-                <div className={styles._5__container___containerIcon}>
-                    <FaCog className={styles._5__container___containerIcon____icon}/>
+                <div className={styles._5__container}>
+                    <div className={styles._5__container___containerIcon}>
+                        <FaCog className={styles._5__container___containerIcon____icon}/>
+                    </div>
+                    <h2 className={styles._5__container___title}>Ux Design</h2>
+                    <p className={styles._5__container___subtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie. Adipiscing vitae vel quam proin eget mauris eget. Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie.</p>
                 </div>
-                <h2 className={styles._5__container___title}>Ux Design</h2>
-                <p className={styles._5__container___subtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie. Adipiscing vitae vel quam proin eget mauris eget. Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molestie.</p>
             </div>
 
         </section>
