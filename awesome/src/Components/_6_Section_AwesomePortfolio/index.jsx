dc308a15079bd3acccb0365124bcbe19a0ac087f
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-07-29 08:18:14 -0300

    Awesome Portfolio Desktop Ok

--- awesome/src/Components/_6_Section_AwesomePortfolio/index.jsx
+++ awesome/src/Components/_6_Section_AwesomePortfolio/index.jsx
@@ -1,20 +1,23 @@
 import styles from './AwesomePortfolio.module.scss'
 
+
 function AwesomePortfolio () {
     return (
         <section className={styles._6}>
 
             <h2 className={styles._6__title}><span className={styles._6__title___span}>awesome</span> portofolio</h2>
             
-            <div className={styles.img1}></div>
-            <div className={styles.img2}></div>
-            <div className={styles.img3}></div>
-            <div className={styles.img4}></div>
-
-            <div className={styles.img3}></div>
-            <div className={styles.img4}></div>
-            <div className={styles.img2}></div>
-            <div className={styles.img1}></div>
+                <div className={styles._6__containerDesktop}>
+                    <div className={styles._6__img1}></div>
+                    <div className={styles._6__img2}></div>
+                    <div className={styles._6__img3}></div>
+                    <div className={styles._6__img4}></div>
+                    <div className={styles._6__img3}></div>
+                    <div className={styles._6__img4}></div>
+                    <div className={styles._6__img2}></div>
+                    <div className={styles._6__img1}></div>
+                </div>
+            
         </section>
     )
 }
