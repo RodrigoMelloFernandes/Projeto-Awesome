bbf78bf9c7842905c55dd392c4e877044c3decdd
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-07-29 17:49:16 -0300

    Animations implementing almoust done

--- awesome/src/App.js
+++ awesome/src/App.js
@@ -2,14 +2,16 @@ import Header from "./Components/_1_Header";
 import Main from './Components/_2_Main';
 import WeAreAwesome from './Components/_3_Section_WeAreAwesome';
 import AwesomeTeam from './Components/_4_Section_AwesomeTeam';
-import OurAwesomeThings from './Components/_5_Section_OurAwesomeThings'
-import AwesomePortfolio from './Components/_6_Section_AwesomePortfolio'
-import ContactAwesome from './Components/_7_Footer_ContactAwesome'
+import OurAwesomeThings from './Components/_5_Section_OurAwesomeThings';
+import AwesomePortfolio from './Components/_6_Section_AwesomePortfolio';
+import ContactAwesome from './Components/_7_Footer_ContactAwesome';
 
+import { AnimateSharedLayout } from "framer-motion";
 
 function App() {
   return (
     <>
+      <AnimateSharedLayout>
       <Header/>
       <Main/>
       <WeAreAwesome/>
@@ -17,6 +19,7 @@ function App() {
       <OurAwesomeThings/>
       <AwesomePortfolio/>
       <ContactAwesome/>
+      </AnimateSharedLayout>
     </>
   );
 }
--- awesome/src/Components/_2_Main/index.jsx
+++ awesome/src/Components/_2_Main/index.jsx
@@ -1,15 +1,27 @@
-import styles from './Main.module.scss'
-
+import styles from './Main.module.scss';
+import { motion } from 'framer-motion'
+ 
 function Main () {
+    
     return (
         <main className={styles._2}>
             <div className={styles._2__containerDesktop}>
+                <motion.h2
+                    initial={{ opacity: 0}}
+                    animate={{ opacity: 1}}
+                    transition={{ delay: .8 , duration: 1 }}
+                >
                 <h2 className={styles._2__title}>We make templates that are <span className={styles._2__title___span}>awesome</span></h2>
+                </motion.h2>
+
                 <p className={styles._2__subtitle}>Download, edit and aplly this awesome for yout website</p>
                 <button className={styles._2__button}>Get Started</button>
             </div>
         </main>
     )
 }
+
+
+
 
 export default Main
