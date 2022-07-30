2f0e8ef8f817e4534641206346f4f862ee47c804
RodrigoF <fernandes.rodrigo.p1@gmail.com>
2022-07-30 09:27:04 -0300

    Add motion to containers awesometeam

--- awesome/src/Components/_4_Section_AwesomeTeam/index.jsx
+++ awesome/src/Components/_4_Section_AwesomeTeam/index.jsx
@@ -4,36 +4,51 @@ import teamimg3 from '../../Images/team-img3.jpg';
 import teamimg4 from '../../Images/team-img4.jpg';
 import styles from './AwesomeTeam.module.scss';
 
+import { motion } from 'framer-motion';
+
 function AwesomeTeam () {
     return(
         <section className={styles._4}>
-            <h2 className={styles._4__title}><span className={styles._4__title___span}>awesome</span> team</h2>
+            <motion.h2 
+                initial={{ scale : 0}}
+                animate={{ scale: [ .3, 1.05 , .9, 1] }}
+                transition={{ duration : 1.5}}
+
+            className={styles._4__title}><span className={styles._4__title___span}>awesome</span> team</motion.h2>
 
             <div className={styles._4__containerDesktop}>
-                <div className={styles._4__container}>
+                <motion.div className={styles._4__container}
+                    whileHover={{ opacity: .3, transition: 1, y : -5}}
+                >
                     <img className={styles._4__container___img} src={teamimg1} alt="teamimg1" />
                     <h2 className={styles._4__container___title}>tracy</h2>
                     <h3 className={styles._4__container___subtitle}>Designer</h3>
                     <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
-                </div>
-                <div className={styles._4__container}>
+                </motion.div>
+                <motion.div className={styles._4__container}
+                    whileHover={{ opacity: .3, transition: 1, y : -5}}
+                >
                     <img className={styles._4__container___img} src={teamimg2} alt="teamimg2" />
                     <h2 className={styles._4__container___title}>mary</h2>
                     <h3 className={styles._4__container___subtitle}>Developer</h3>
                     <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
-                </div>
-                <div className={styles._4__container}>
+                </motion.div>
+                <motion.div className={styles._4__container}
+                    whileHover={{ opacity: .3, transition: 1, y : -5}}
+                >
                     <img className={styles._4__container___img} src={teamimg3} alt="teamimg3" />
                     <h2 className={styles._4__container___title}>julia</h2>
                     <h3 className={styles._4__container___subtitle}>Director</h3>
                     <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
-                </div>
-                <div className={styles._4__container}>
+                </motion.div>
+                <motion.div className={styles._4__container}
+                    whileHover={{ opacity: .3, transition: 1, y : -5}}
+                >
                     <img className={styles._4__container___img} src={teamimg4} alt="teamimg4" />
                     <h2 className={styles._4__container___title}>linda</h2>
                     <h3 className={styles._4__container___subtitle}>Manager</h3>
                     <p className={styles._4__container___content}>Lorem ipsum dolor sit amet, consectetur adipiscing elitquisque tempus ac eget diam et laoreet phasellus ut nisi id leo molest.</p>
-                </div>
+                </motion.div>
             </div>
         </section>
     )
